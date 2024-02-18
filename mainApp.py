4258f73ce5aef03fbfd2f9a6e0f1485c40d53dd2
testruncoder <97300656+testruncoder@users.noreply.github.com>
2024-02-17 20:28:07 -0700

    Update mainApp.py

--- mainApp.py
+++ mainApp.py
@@ -54,8 +54,8 @@ selectbox_options=[
 sample_imgs_dict={
                 'Scroll down to select an image': None,
                 'License Plate': r"sample_images/image_demo_licnesePlate.png",
-                'Receipt': r"/sample_images/img_demo_receipt.jpg" ,
-                'Prescription': r"/sample_images/medical-prescription-ocr.webp",
+                'Receipt': r"sample_images/img_demo_receipt.jpg" ,
+                'Prescription': r"sample_images/medical-prescription-ocr.webp",
                   }
 radio_btn_options=['🖼️ Upload an Image','🗃️ Select an Image Sample']
 
@@ -80,8 +80,8 @@ radio_btn=container1.radio('⭐ Choose a method:',
 img_width=480
 with container3:
     st.markdown('')
-    with st.expander('Image Width',expanded=False):
-        img_width=st.slider('Choose the image width:', min_value=320,max_value=800, value=480, step=20,
+    with st.expander('↔️ Image Width',expanded=False):
+        img_width=st.slider('Choose the image width:', min_value=200,max_value=760, value=480, step=20,
                             help='Default = 480',
                             key='slider_imgwidth0',
                             )
@@ -99,17 +99,15 @@ if radio_btn==radio_btn_options[1]:  #'Select an Image Sample'
     if sample_image is not None:  # That is, a user selects an image from st.sidebar.selectbox() other than its placeholder value.
         img_uploaded_name=sample_imgs_dict[sample_image]    
 
-    # if img_uploaded_name != '':      
                                 # img_np=np.array(img)
         img_np=cv2.imread(img_uploaded_name)
-        # img_np=img_np[:,:,::-1]
-        # img_np=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
+                                # img_np=img_np[:,:,::-1]
+        img_np=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
         st.image(img_np, width=img_width,caption=f'{sample_image}')
 
 # # Upload an input image
 if radio_btn==radio_btn_options[0]:  # '🖼️ Upload an Image'
     img_uploaded=container1.file_uploader('🖼️ Upload an image file', type=['jpg','jpeg','png','webp'])
-
 
 if (radio_btn==radio_btn_options[0]) and (img_uploaded is not None):  # '🖼️ Upload an Image','Select an Image Sample'
     st.subheader('🖼️ Uploaded Image:')
@@ -117,9 +115,9 @@ if (radio_btn==radio_btn_options[0]) and (img_uploaded is not None):  # '🖼️
     img=Image.open(img_uploaded)
     # # Convert a file from st.file_uploader() to cv2 format;
                             # img_np=np.array(img)
-    # img_np=cv2.imread(img_uploaded.name)
+    img_np=cv2.imread(img_uploaded.name)
     # img_np=img_np[:,:,::-1]
-    # img_np=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
+    img_np=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
     img_uploaded_name=img_uploaded.name
 
 with st.form('my form'):
@@ -128,65 +126,67 @@ with st.form('my form'):
         if (img_uploaded is None) and (img_uploaded_name is None):
             st.warning('Please upload an image file in the left side panel.')
         else:
-            # st.image(img_uploaded, caption=f'{img_uploaded.name}')
-            # img=Image.open(img_uploaded)
+                                    # st.image(img_uploaded, caption=f'{img_uploaded.name}')
+                                    # img=Image.open(img_uploaded)
 
-            # # # Convert a file from st.file_uploader() to cv2 format;
-            #                         # img_np=np.array(img)
-            # img_np=cv2.imread(img_uploaded.name)
-            # img_np=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
-            if submitted:
-                show_info1=st.empty()
-                show_info1.info('Applying OCR to the image (it may take a while)...')
-                result=reader.readtext(img_uploaded_name)
-                show_info1.empty()
+                                    # # # Convert a file from st.file_uploader() to cv2 format;
+                                    #                         # img_np=np.array(img)
+                                    # img_np=cv2.imread(img_uploaded.name)
+                                    # img_np=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
+            # if submitted:
+            show_info1=st.empty()
+            show_info1.info('Applying OCR to the image (it may take a while)...')
+            result=reader.readtext(img_uploaded_name)
+            show_info1.empty()
 
-                with st.expander('Results: text and probability', expanded=False):
-                    for (bbox,text,prob) in result:
-                        st.markdown(f'Text: {text}, Probability: {prob}')
-                    for res in result:
-                        st.markdown(f'Text: {res[1]}, Coordinates: {res[0]}')
+            with st.expander('Results: text and probability', expanded=False):
+                for (bbox,text,prob) in result:
+                    st.markdown(f'Text: {text}, Probability: {prob}')
+                for res in result:
+                    st.markdown(f'Text: {res[1]}, Coordinates: {res[0]}')
 
-                # font=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-                font=cv2.FONT_HERSHEY_SIMPLEX
-                for reslt in result:
-                    # # should take integers of a tuple - Ver0_1 (02/15/2024)
-                    top_left=tuple(reslt[0][0])  # e.g., (102, 606)
-                    top_left=tuple(map(int, top_left))
-                    bottom_right=tuple(reslt[0][2])
-                    bottom_right=tuple(map(int, bottom_right))
-                    text=re.sub("""[."'}]""","",reslt[1])
-                    img_np=cv2.rectangle(img_np,top_left,bottom_right, 
-                                        (0,255,0), # (0,255,100),
-                                        2)
-                    # img_np=cv2.putText(img_np,text,bottom_right,font, 1.5, (0,255,0),
-                    #                 2,  # 4
-                    #                 cv2.LINE_AA
-                    #                 )
+            # font=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+            font=cv2.FONT_HERSHEY_SIMPLEX
+            for reslt in result:
+                # # should take integers of a tuple - Ver0_1 (02/15/2024)
+                top_left=tuple(reslt[0][0])  # e.g., (102, 606)
+                top_left=tuple(map(int, top_left))
+                bottom_right=tuple(reslt[0][2])
+                bottom_right=tuple(map(int, bottom_right))
+                text=re.sub("""[."'}]""","",reslt[1])
+                img_np=cv2.rectangle(img_np,top_left,bottom_right, 
+                                    (0,255,0), # (0,255,100),
+                                    2)
+                # img_np=cv2.putText(img_np,text,bottom_right,font, 1.5, (0,255,0),
+                #                 2,  # 4
+                #                 cv2.LINE_AA
+                #                 )
 
-                                    # # Rotate image colors from BGR to RGB -- NOT QUITE WORKING (Ver0) (02/15/2024)
-                                    # img_rgb=img_np[:,:,::-1]
-            
-                st.subheader('📋 Result:')
-                st.image(img_np, channels='RGB')
-                c1,c2,c3=st.columns((6,1,2))
-                c1.markdown('📜*Recognized Text:*')
-                c3.markdown('🎯 *Confidence Level:*')
-                for (_,text,prob) in result:
-                    c1.markdown(text)
-                    c3.markdown(f'{prob*100:0.2f}%')
-                st.divider()
-                with st.expander('View the image in a graph',expanded=False):
-                    plt.figure(figsize=(10,10))
-                    plt.imshow(img_np)
-                    st.pyplot(plt.gcf())
-                    st.markdown('')
+                                # # Rotate image colors from BGR to RGB -- NOT QUITE WORKING (Ver0) (02/15/2024)
+                                # img_rgb=img_np[:,:,::-1]
+        
+            st.subheader('📋 Result:')
+            st.image(img_np, width=img_width, channels='RGB')
+            c1,c2,c3=st.columns((6,1,2))
+            c1.markdown('📜*Recognized Text:*')
+            c3.markdown('🎯 *Confidence Level:*')
+            for (_,text,prob) in result:
+                c1.markdown(text)
+                c3.markdown(f'{prob*100:0.2f}%')
+            st.divider()
+            with st.expander('View the image in a graph',expanded=False):
+                plt.figure(figsize=(10,10))
+                plt.imshow(img_np)
+                c1,c2,c3=st.columns((1,10,1))
+                c2.pyplot(plt.gcf())
+                st.markdown('')
     # ----------------------------- END OF st.form() -----------------------------------
 
 for i in range(12):
     st.sidebar.markdown('')
 
 # # Coloring st.button() 
+# Inspired by JY_pyTools\stMyCalendar\JY_st_timeGridList0_6a.py- Ver02c (01/23/2024)
 with st.sidebar.expander('🎨 Color for buttons', expanded=False):
 # with container_colorBtn:
     primaryColor=st.color_picker('Click color to change', "#008CF9")  # "#00f900")
